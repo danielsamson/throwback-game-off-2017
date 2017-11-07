d3d496150441016fdc091a80f461a57854ea49e3
Daniel Samson <daniel.l.samson@gmail.com>
2017-11-07 14:36:32 +0000

    first

---
+++ Assets/ClickToMove.cs
@@ -0,0 +1,32 @@
+﻿﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+namespace CompleteProject {
+    
+	public class ClickToMove : MonoBehaviour {
+
+		public Transform goal;
+        private NavMeshAgent agent;
+
+		void Start()
+		{
+			agent = GetComponent<NavMeshAgent>();
+			agent.destination = goal.position;
+		}
+
+		void Update()
+		{
+			if (Input.GetMouseButtonDown(0))
+			{
+				RaycastHit hit;
+
+				if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit, 100))
+				{
+					agent.destination = hit.point;
+				}
+			}
+		}
+	}
+}
